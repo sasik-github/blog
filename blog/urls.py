bcf774a20c0136a019b1ae65de00c5a7c1ab382f
sasik-github <dart_sas@mail.ru>
2013-07-26 20:43:30 +0700

    start blog project on django

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.defaults import *
+from blog.models import *
+from blog.views import PostView, Main, ArchiveMonth
+
+urlpatterns = patterns("mysite.blog.views",
+   (r"^post/(?P<dpk>\d+)/$"          , PostView.as_view(), {}, "post"),
+   (r"^archive_month/(\d+)/(\d+)/$"  , ArchiveMonth.as_view(), {}, "archive_month"),
+   (r"^$"                            , Main.as_view(), {}, "main"),
+   # (r"^delete_comment/(\d+)/$"       , "delete_comment", {}, "delete_comment"),
+)
